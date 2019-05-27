13f70f5c22881f1b8c0e41159bdb2981b016d47a
Slad3Wils0n <Paul@github.com>
2019-05-27 12:06:10 +0200

    pointer

--- usuario.js
+++ usuario.js
@@ -1,30 +1,24 @@
 window.addEventListener("load", iniciar)
-console.log("nepe")
-alert("nepe")
-
+alert("entra")
 function iniciar(){
-    console.log("nepe")
     document.getElementById("perfil").addEventListener("click", mperfil)
     document.getElementById("Comentarios").addEventListener("click", mcomen)
     document.getElementById("Compras").addEventListener("click", mcomp)
 }
 
 function mperfil(){
-    console.log("nepe")
     document.getElementById("comments").style.display="none";
     document.getElementById("sales").style.display="none";
     document.getElementById("profile").style.display="inline";
 }
 
 function mcomen(){
-    console.log("nepe")
     document.getElementById("profile").style.display="none";
     document.getElementById("sales").style.display="none";
     document.getElementById("comments").style.display="inline";
 }
 
 function mcomp(){
-    console.log("nepe")
     document.getElementById("comments").style.display="none";
     document.getElementById("profile").style.display="none";
     document.getElementById("sales").style.display="inline";
